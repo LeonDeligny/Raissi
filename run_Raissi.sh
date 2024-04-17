af1d9053498f82508b2eb90204781eded90cc88c
Deligny <61823445+LeonDeligny@users.noreply.github.com>
2024-04-17 09:11:33 +0200

    Update run_Raissi.sh

--- run_Raissi.sh
+++ run_Raissi.sh
@@ -3,6 +3,7 @@
 #SBATCH --output=python_job_%j.out
 #SBATCH --error=python_job_%j.err
 #SBATCH --nodes=1                    # Number of nodes
+#SBATCH --gres=gpu:1                 # Request GPU
 #SBATCH --ntasks-per-node=32         # Number of tasks per node
 #SBATCH --mem=100G                   # Memory per node
 #SBATCH --time=00:10:00              # Time (hh:mm:ss)
