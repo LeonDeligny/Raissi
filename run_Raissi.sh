6140770c889b127fde3bc47d79ce430116d3e042
Deligny <61823445+LeonDeligny@users.noreply.github.com>
2024-04-17 14:42:40 +0200

    Update run_Raissi.sh

--- run_Raissi.sh
+++ run_Raissi.sh
@@ -5,7 +5,6 @@
 #SBATCH --nodes=1                    # Number of nodes
 #SBATCH --ntasks-per-node=1          # Number of tasks per node
 #SBATCH --cpus-per-task=1            # Number of CPUs per task
-#SBATCH --mem=100G                   # Memory per node
 #SBATCH --time=00:10:00              # Time (hh:mm:ss)
 #SBATCH --gres=gpu:1 
 #SBATCH --partition=gpu 
