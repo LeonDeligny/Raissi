7cfb9314fffb781090aedaa00b47d74e8662987d
Deligny <61823445+LeonDeligny@users.noreply.github.com>
2024-04-17 09:02:00 +0200

    Update run_Raissi.sh

--- run_Raissi.sh
+++ run_Raissi.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=python_job
 #SBATCH --output=python_job_%j.out
 #SBATCH --error=python_job_%j.err
-#SBATCH --nodes=2                    # Number of nodes
+#SBATCH --nodes=1                    # Number of nodes
 #SBATCH --ntasks-per-node=32         # Number of tasks per node
 #SBATCH --mem=100G                   # Memory per node
-#SBATCH --time=10:00:00              # Time (hh:mm:ss)
+#SBATCH --time=00:10:00              # Time (hh:mm:ss)
 
 
 # Navigate to the directory containing the script and modules
